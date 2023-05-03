02e74be34694466941490baaeb41b9406c1d5572
Danyal Imran <dan.imran97@gmail.com>
2023-05-03 12:09:48 -0400

    completed implementing Nasa APOD API and basic styling / html semantics / mobile responsiveness using flex.

---
+++ js/main.js
@@ -0,0 +1,24 @@
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch() {
+  const choice = document.querySelector('input').value
+  console.log(choice)
+
+  const url = `https://api.nasa.gov/planetary/apod?api_key=Uxchdu0tuSpAVpf6E3beJRAwsaVJzLaDW1Aa9GLg&date=${choice}`
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      if( data.media_type === 'image' ) {
+        document.querySelector('img').src = data.hdurl
+      } else if(data.media_type === 'video') {
+        document.querySelector('iframe').src = data.url
+      }
+
+      document.querySelector('h3').innerText = data.explanation
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    })
+}
